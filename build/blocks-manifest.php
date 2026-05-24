291cfcd335e25a9caa405f946663eec96c8bfd5a
Christian Studer <cstuder@existenz.ch>
2026-05-24 10:40:14 +0200

    Initial commit.

---
+++ build/blocks-manifest.php
@@ -0,0 +1,26 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'infomail-kalender-block' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/infomail-kalender-block',
+		'version' => '0.1.0',
+		'title' => 'Infomail Kalender Block',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Example block scaffolded with Create Block tool.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'infomail-kalender-block',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'render' => 'file:./render.php',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ build/infomail-kalender-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '13cb8b563cbb83553b00');
